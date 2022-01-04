eea7c370441b0cbf7c5b15fcd7e680b9cd819df9
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-01-04 21:19:57 +0100

    chore: page with simple button that initializes a qr code scanner page

--- app/src/main/java/com/sacr/sacr_mobile_client/fragments/ScanButtonFragment.kt
+++ app/src/main/java/com/sacr/sacr_mobile_client/fragments/ScanButtonFragment.kt
@@ -4,24 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.sacr.sacr_mobile_client.R
 
-class ScanButtonFragment(var manager: FragmentManager) : Fragment() {
+class ScanButtonFragment(private var manager: FragmentManager) : Fragment() {
 
-    lateinit var layout: FrameLayout
+    private lateinit var layout: FrameLayout
+    private lateinit var scanButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val root: View = inflater.inflate(R.layout.scan_button_layout, container, false)
 
         this.layout = root.findViewById(R.id.fragment_scan_button)
+        this.scanButton = root.findViewById(R.id.scan_button)
+        this.scanButton.setOnClickListener {
+            val transaction: FragmentTransaction = manager.beginTransaction()
+            val scanCodeFragment = ScanCodeFragment(manager)
+            transaction.replace(R.id.fragment_container, scanCodeFragment)
+            transaction.commit()
+        }
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return root
     }
 }
