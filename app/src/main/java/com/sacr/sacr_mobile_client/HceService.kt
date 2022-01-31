6dca471de4a13c8c910f5bd87d8553c00022c489
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2022-01-31 10:59:30 +0100

    fix hce service

--- app/src/main/java/com/sacr/sacr_mobile_client/HceService.kt
+++ app/src/main/java/com/sacr/sacr_mobile_client/HceService.kt
@@ -9,6 +9,7 @@ class HceService : HostApduService() {
     val selectionByte = 11
     val part1 = 1
     val part2 = 2
+    val part3 = 3
 
     /**
      * This method is called whenever a NFC reader sends
@@ -19,13 +20,15 @@ class HceService : HostApduService() {
      * half-duplex: the NFC reader sends you a command APDU,
      * and it waits for you to send a response APDU in return.
      */
-    override fun processCommandApdu(p0: ByteArray?, p1: Bundle?): ByteArray? {
+    override fun processCommandApdu(p0: ByteArray?, p1: Bundle?): ByteArray {
         return if (p0 != null) {
             val code = GreenPassStorage.getGreenPass(this)
-            val half = code.length / 2
+            val firstBlock: Int = code.length / 3
+            val secondBlock: Int = firstBlock * 2
             when (p0[selectionByte].toInt()) {
-                part1 -> code.substring(0, half).toByteArray()
-                part2 -> code.substring(half, code.length).toByteArray()
+                part1 -> code.substring(0, firstBlock).toByteArray()
+                part2 -> code.substring(firstBlock, secondBlock).toByteArray()
+                part3 -> code.substring(secondBlock, code.length).toByteArray()
                 else -> error.toByteArray()
             }
         } else {
